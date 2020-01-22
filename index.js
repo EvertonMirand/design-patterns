cbf6210af13188cbeb1f2fad54c2ffee0c2da187
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-01-21 21:31:57 -0300

    Singleton pattern

--- index.js
+++ index.js
@@ -13,5 +13,21 @@ class Suv extends Car {
 	}
 }
 
+class SingletonCar extends Car {
+	static instance = null;
+	constructor(doors, engine, color) {
+		if (SingletonCar.instance) {
+			return SingletonCar.instance;
+		}
+		super(doors, engine, color);
+		return this;
+	}
+}
+
 const civic = new Car(4, 'v6', 'grey');
 const cx5 = new Suv(4, 'v8', 'red', 4);
+const civic2 = new SingletonCar(4, 'v6', 'grey');
+
+console.log(civic);
+console.log(cx5);
+console.log(civic2);
