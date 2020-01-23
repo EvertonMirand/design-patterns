4b6094333f4482add096c760e26ac8b5a57649a8
EvertonMirand <everton.mirandav@gmail.com>
2020-01-23 09:51:43 -0300

    Observer

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ Object.assign(Car.prototype, carMixin);
 const carFactory = new CarFactory();
 const suvFactory = new SuvFactory();
 
+// Abstract
 const autoManufacturer = (type, model) => {
   switch (type) {
     case "car":
@@ -84,3 +85,39 @@ console.log(civic2);
 console.log(suv);
 
 civic.revEngine();
+
+// Observer
+class Car {
+  constructor(gas) {
+    this.gas = gas;
+  }
+
+  setGasLevel(val) {
+    this.gas = val;
+    this.notifyAll();
+  }
+
+  register(observer) {
+    this.actions.push(observer);
+  }
+
+  unregister(observer) {
+    this.actions.remove.filter(function(el) {
+      return el !== observer;
+    });
+  }
+
+  notifyAll() {
+    return this.actions.forEach(
+      function(el) {
+        el.update(this);
+      }.bind(this)
+    );
+  }
+}
+
+class cusumption {
+  update(car) {
+    car.gas = car.gas + 1;
+  }
+}
